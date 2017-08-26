fd4e300c34085560f0ada31a9135658898b73aaa
Nurbek.Izbassar <nchecker95@gmail.com>
2017-08-26 14:53:08 +0600

    create environment for react

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+    static path = '/';
+
+    render() {
+        return (
+            <h1>Hello World from App</h1>
+        );
+    }
+}
